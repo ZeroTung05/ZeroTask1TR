3574365edca8232eec9e825837cef6068ec807c7
ZeroTung05 <s.ch2605@mail.ru>
2023-05-21 19:34:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String Zeroo = "ZeroTung is the best";
+
+        System.out.println(Zeroo.charAt(Zeroo.length() /2));
+
+    }
+}
---
+++ src/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    public static void main(String[] args) {
+        String Zeroo = "ZeroTung is the best";
+        for (int i = Zeroo.length()-1; i >=0 ; i--) {
+            System.out.print(Zeroo.charAt(i));
+
+        }
+
+
+    }
+}
